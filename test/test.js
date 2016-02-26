220b83ef5ada3cbbb310fe1129efba0199f44e5f
Alex <j201.alex@gmail.com>
2016-02-26 01:00:42 -0500

    Tests for missing target/link extension

--- test/test.js
+++ test/test.js
@@ -2,13 +2,13 @@ var ws = require('../lib/windows-shortcuts');
 var tape = require('tape');
 var tmp = require('tmp');
 var touch = require("touch");
+var path = require("path");
 
 tmp.setGracefulCleanup();
 
-var target = tmp.tmpNameSync({postfix: ".txt"});
-touch.sync(target);
-
 tape("create->query", function(t) {
+	var target = tmp.tmpNameSync({postfix: ".txt"});
+	touch.sync(target);
 	var lnkName = tmp.tmpNameSync({postfix: ".lnk"});
 	var lnkOpts = {
 		target : target,
@@ -31,6 +31,8 @@ tape("create->query", function(t) {
 });
 
 tape("create->edit->query", function(t) {
+	var target = tmp.tmpNameSync({postfix: ".txt"});
+	touch.sync(target);
 	var lnkName = tmp.tmpNameSync({postfix: ".lnk"});
 	var lnkOpts = {
 		target : target,
@@ -59,6 +61,55 @@ tape("create->edit->query", function(t) {
 				});
 				t.end();
 			});
+		});
+	});
+});
+
+tape("create->query without target extension", function(t) {
+	var target = tmp.tmpNameSync();
+	touch.sync(target);
+	var lnkName = tmp.tmpNameSync({postfix: ".lnk"});
+	var lnkOpts = {
+		target : target,
+		args : '2 "baz quux"',
+		workingDir: "C:\\Windows",
+		runStyle : ws.MIN,
+		hotkey: 100,
+		desc : "Does cool stuff."
+	};
+	ws.create(lnkName, lnkOpts, function(err) {
+		t.notOk(err, "No errors on create");
+		ws.query(lnkName, function(err, opts) {
+			t.notOk(err, "No errors on query");
+			Object.keys(lnkOpts).forEach(function(k) {
+				t.equal(lnkOpts[k], opts[k]);
+			});
+			t.end();
+		});
+	});
+});
+
+tape("create->query in directory", function(t) {
+	var target = tmp.tmpNameSync({postfix: ".txt"});
+	var targetObj = path.parse(target);
+	touch.sync(target);
+	var lnk = tmp.dirSync();
+	var lnkOpts = {
+		target : target,
+		args : '2 "baz quux"',
+		workingDir: "C:\\Windows",
+		runStyle : ws.MIN,
+		hotkey: 100,
+		desc : "Does cool stuff."
+	};
+	ws.create(lnk.name, lnkOpts, function(err) {
+		t.notOk(err, "No errors on create");
+		ws.query(lnk.name + "\\" + targetObj.name + ".lnk", function(err, opts) {
+			t.notOk(err, "No errors on query");
+			Object.keys(lnkOpts).forEach(function(k) {
+				t.equal(lnkOpts[k], opts[k]);
+			});
+			t.end();
 		});
 	});
 });
