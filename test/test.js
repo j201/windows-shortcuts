6c9221d8b104307ff6dcf8a149e71e2ba82319de
Alex <j201.alex@gmail.com>
2016-03-03 01:01:23 -0500

    Add environment variable support
    With tests for added features and spaces in dirs

--- test/test.js
+++ test/test.js
@@ -13,7 +13,7 @@ var filesToDelete = [];
 onExit(function() {
 	filesToDelete.forEach(function(f) {
 		try {
-			fs.unlinkSync(f);
+			fs.unlinkSync(f.replace("%TEMP%", process.env.TEMP));
 		} catch (e) {
 			console.log('Deleting ' + f + ' failed.');
 		}
@@ -131,6 +131,70 @@ tape("create->query in directory", function(t) {
 			t.notOk(err, "No errors on query");
 			Object.keys(lnkOpts).forEach(function(k) {
 				t.equal(lnkOpts[k], opts[k]);
+			});
+			t.end();
+		});
+	});
+});
+
+tape("create->query in directory with spaces", function(t) {
+	var tmpDir = tmp.dirSync();
+	var spacedDir = tmpDir.name + "\\spaced dir";
+	fs.mkdirSync(spacedDir);
+	var target = tmp.tmpNameSync({ dir: spacedDir, postfix: ".txt" });
+	var targetObj = path.parse(target);
+	touch.sync(target);
+	filesToDelete.push(target);
+	var lnk = tmp.dirSync({dir: spacedDir});
+	var lnkOpts = {
+		target : target,
+		args : '2 "baz quux"',
+		workingDir: "C:\\Windows",
+		runStyle : ws.MIN,
+		hotkey: 100,
+		desc : "Does cool stuff."
+	};
+	ws.create(lnk.name, lnkOpts, function(err) {
+		t.notOk(err, "No errors on create");
+		var lnkName = lnk.name + "\\" + targetObj.name + ".lnk";
+		if (!err) filesToDelete.push(lnkName);
+		ws.query(lnkName, function(err, opts) {
+			t.notOk(err, "No errors on query");
+			Object.keys(lnkOpts).forEach(function(k) {
+				t.equal(lnkOpts[k], opts[k]);
+			});
+			t.end();
+		});
+	});
+});
+
+tape("create->query with environment variables", function(t) {
+	var target = tmp.tmpNameSync({ dir: "%TEMP%", postfix: ".txt" });
+	t.ok(target.indexOf("%TEMP%") !== -1, "tmp doesn't expand env variables");
+	var expandedTarget = target.replace("%TEMP%", process.env.TEMP);
+	touch.sync(expandedTarget);
+	filesToDelete.push(target);
+	var lnkName = tmp.tmpNameSync({ dir: "%TEMP%", postfix: ".lnk" });
+	var lnkOpts = {
+		target : target,
+		args : '2 "baz quux"',
+		workingDir: "^%SOMEDIR^%",
+		runStyle : ws.MIN,
+		hotkey: 100,
+		desc : "Does cool stuff."
+	};
+	ws.create(lnkName, lnkOpts, function(err) {
+		t.notOk(err, "No errors on create");
+		if (!err) filesToDelete.push(lnkName);
+		ws.query(lnkName, function(err, opts) {
+			t.notOk(err, "No errors on query");
+			Object.keys(lnkOpts).forEach(function(k) {
+				if (k === 'workingDir')
+					t.equal(opts[k], "%SOMEDIR%");
+				else if (k === 'target')
+					t.equal(opts[k], expandedTarget);
+				else
+					t.equal(lnkOpts[k], opts[k]);
 			});
 			t.end();
 		});
