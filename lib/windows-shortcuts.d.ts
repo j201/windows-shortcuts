6776a291b5ce659779f695af853a6d3257a1810e
cbartondock <cbartondock@gmail.com>
2020-01-09 15:57:02 -0500

    increase typescript compatibility

--- lib/windows-shortcuts.d.ts
+++ lib/windows-shortcuts.d.ts
@@ -15,10 +15,10 @@ interface ShortcutOptions {
     icon?: string;
     /** An optional index for the image in the icon file. **/
     iconIndex?: number;
-    /** 
-     * A number representing a hotkey.  
-     * To find out this value, create a shortcut manually and use ws.query on it. 
-     * Sorry about that inconvenience, but there isn't any more documentation either with shortcut.exe or from Microsoft.  
+    /**
+     * A number representing a hotkey.
+     * To find out this value, create a shortcut manually and use ws.query on it.
+     * Sorry about that inconvenience, but there isn't any more documentation either with shortcut.exe or from Microsoft.
      **/
     hotkey?: number;
     /** A string description of the shortcut. **/
@@ -35,9 +35,9 @@ interface CreateOrEditCallback {
 interface QueryCallback {
     /**
      * @param error A string error message if there was an error, otherwise null
-     * 
-     * @param options 
-     * The options set on the shortcut with the same properties as above, 
+     *
+     * @param options
+     * The options set on the shortcut with the same properties as above,
      * except an additional property expanded is added which contains the file name properties with any environment variables expanded.
      * For example, if options.target is "%WINDIR%/foo.exe", options.expanded.target would be "C:/Windows/foo.exe".
      */
@@ -46,41 +46,41 @@ interface QueryCallback {
 
 /**
  * Creates a new shortcut.
- * 
- * @param path 
- * The file path to the new shortcut. 
- * This can be a folder, in which case a .lnk file will be created in that folder 
- * with the name of the target file, or the name of a .lnk file, which will be created. 
- * Note that a folder that does not exist will not be created. 
- * Environment variables like %WINDIR% can be used, but they will be expanded when the shortcut is created. 
+ *
+ * @param path
+ * The file path to the new shortcut.
+ * This can be a folder, in which case a .lnk file will be created in that folder
+ * with the name of the target file, or the name of a .lnk file, which will be created.
+ * Note that a folder that does not exist will not be created.
+ * Environment variables like %WINDIR% can be used, but they will be expanded when the shortcut is created.
  * If you want them to be expanded when the shortcut is clicked, use carets before the percent signs: ^%WINDIR^%.
- * 
- * @param target 
+ *
+ * @param target
  * If a string is passed as the second parameter, it is used as the options.target value (see above).
- * 
- * @param callback 
- * A function to be executed when ws.create is finished executing. 
+ *
+ * @param callback
+ * A function to be executed when ws.create is finished executing.
  */
-export function create(path: string, target: string, callback?: CreateOrEditCallback);
+export function create(path: string, target: string, callback?: CreateOrEditCallback): void;
 
 /**
  * Creates a new shortcut.
- * 
- * @param path 
- * The file path to the new shortcut. 
- * This can be a folder, in which case a .lnk file will be created in that folder 
- * with the name of the target file, or the name of a .lnk file, which will be created. 
- * Note that a folder that does not exist will not be created. 
- * Environment variables like %WINDIR% can be used, but they will be expanded when the shortcut is created. 
+ *
+ * @param path
+ * The file path to the new shortcut.
+ * This can be a folder, in which case a .lnk file will be created in that folder
+ * with the name of the target file, or the name of a .lnk file, which will be created.
+ * Note that a folder that does not exist will not be created.
+ * Environment variables like %WINDIR% can be used, but they will be expanded when the shortcut is created.
  * If you want them to be expanded when the shortcut is clicked, use carets before the percent signs: ^%WINDIR^%.
- * 
- * @param options 
+ *
+ * @param options
  * An object with optional parameters
- * 
- * @param callback 
- * A function to be executed when ws.create is finished executing. 
+ *
+ * @param callback
+ * A function to be executed when ws.create is finished executing.
  */
-export function create(path: string, options: ShortcutOptions, callback?: CreateOrEditCallback);
+export function create(path: string, options: ShortcutOptions, callback?: CreateOrEditCallback): void;
 
 /**
  * Edits an existing shortcut, applying new options.
@@ -88,12 +88,12 @@ export function create(path: string, options: ShortcutOptions, callback?: Create
  * @param options An object with optional parameters
  * @param callback A function to be executed when ws.edit is finished executing
  */
-export function edit(path: string, options: ShortcutOptions, callback?: CreateOrEditCallback);
+export function edit(path: string, options: ShortcutOptions, callback?: CreateOrEditCallback): void;
 
 /**
  * Collects information about an existing shortcut.
  * @param path The file path to an existing shortcut
  * @param callback A function to be executed when ws.query is finished executing
  */
-export function query(path: string, callback: (error: string | null, options?: ShortcutOptions) => void);
+export function query(path: string, callback: (error: string | null, options?: ShortcutOptions) => void): void;
 
